765dde487d0a23dda3167d55a429debf9def7dad
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-05-28 19:44:24 +0700

    Implement Firebase call and counter functions
    Added functions to listen, set, and get call and counter data from Firebase.

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
+import { ref, onValue, set, get } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+import { db } from "./firebase.js";
 
+// ================= CURRENT CALL =================
+export function listenCall(callback){
+  onValue(ref(db, "current_call"), (snap)=>{
+    callback(snap.val());
+  });
+}
+
+// ================= SET CALL =================
+export function setCall(data){
+  return set(ref(db, "current_call"), data);
+}
+
+// ================= GET COUNTER =================
+export async function getCounter(loket){
+  const snap = await get(ref(db, "counter/" + loket));
+  return snap.exists() ? snap.val() : 0;
+}
+
+// ================= SET COUNTER =================
+export function setCounter(loket, value){
+  return set(ref(db, "counter/" + loket), value);
+}
