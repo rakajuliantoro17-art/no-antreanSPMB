9490a159f9daf261d7ff0abbb214fca845f8a855
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-05-28 12:26:36 +0700

    Refactor firebase.js to use Firebase SDK
    Removed old configuration structure and added Firebase SDK imports and initialization.

--- firebase.js
+++ firebase.js
@@ -1,17 +1,52 @@
-{
-  "queues": {
-    "waiting": {}
-  },
+// Firebase SDK
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 
-  "counters": {
-    "1": {
-      "current": "",
-      "status": "idle"
-    }
-  },
+import {
+  getDatabase,
+  ref,
+  set,
+  get,
+  update,
+  remove,
+  push,
+  onValue
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
-  "display": {
-    "number": "",
-    "counter": ""
-  }
-}
+
+// CONFIG FIREBASE
+const firebaseConfig = {
+
+  apiKey: "ISI_API_KEY",
+
+  authDomain: "ISI_PROJECT.firebaseapp.com",
+
+  databaseURL: "https://ISI_PROJECT-default-rtdb.asia-southeast1.firebasedatabase.app",
+
+  projectId: "ISI_PROJECT",
+
+  storageBucket: "ISI_PROJECT.appspot.com",
+
+  messagingSenderId: "ISI_SENDER_ID",
+
+  appId: "ISI_APP_ID"
+};
+
+
+// INITIALIZE
+const app = initializeApp(firebaseConfig);
+
+// DATABASE
+const db = getDatabase(app);
+
+
+// EXPORT
+export {
+  db,
+  ref,
+  set,
+  get,
+  update,
+  remove,
+  push,
+  onValue
+};
