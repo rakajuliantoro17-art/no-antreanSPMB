27ea81f11c683a45afcb7bd5f11c87554b5a64f3
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-05-28 19:43:47 +0700

    Refactor Firebase database imports and exports
    Removed unused Firebase database methods and updated exports for cleaner architecture.

--- firebase.js
+++ firebase.js
@@ -1,52 +1,21 @@
 // Firebase SDK
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
-
-import {
-  getDatabase,
-  ref,
-  set,
-  get,
-  update,
-  remove,
-  push,
-  onValue
-} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
-
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
 
 // Firebase Config
 const firebaseConfig = {
-
   apiKey: "AIzaSyAW_Wh9ttPQ8vnwgnQFUMMEDc5QqwJe3GQ",
-
   authDomain: "no-antrean-spmb.firebaseapp.com",
-
   databaseURL: "https://no-antrean-spmb-default-rtdb.asia-southeast1.firebasedatabase.app",
-
   projectId: "no-antrean-spmb",
-
   storageBucket: "no-antrean-spmb.firebasestorage.app",
-
   messagingSenderId: "357121462512",
-
   appId: "1:357121462512:web:8fd937bc2a8d2a3894b235"
 };
 
-
-// Initialize Firebase
+// Init Firebase
 const app = initializeApp(firebaseConfig);
-
-// Database
 const db = getDatabase(app);
 
-
-// Export
-export {
-  db,
-  ref,
-  set,
-  get,
-  update,
-  remove,
-  push,
-  onValue
-};
+// Export minimal (CLEAN ARCHITECTURE)
+export { db };
