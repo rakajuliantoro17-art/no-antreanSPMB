6820e1c3bf82c15bb158883df5bb73e31aebfaaa
rakajuliantoro17-art <rakajuliantoro17@guru.sma.belajar.id>
2026-05-28 12:22:52 +0700

    Add initial configuration to firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,17 @@
+{
+  "queues": {
+    "waiting": {}
+  },
 
+  "counters": {
+    "1": {
+      "current": "",
+      "status": "idle"
+    }
+  },
+
+  "display": {
+    "number": "",
+    "counter": ""
+  }
+}
